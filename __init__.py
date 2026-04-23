14a5001c72d89a5ce0c60b15ee756deefe99ad9c
Àngel Àlvarez <angel@nan-tic.com>
2026-04-23 15:52:31 +0200

    initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+
+from trytond.pool import Pool
+
+from . import work
+
+
+def register():
+    Pool.register(
+        work.WorkCycle,
+        module='production_work_shift', type_='model')
